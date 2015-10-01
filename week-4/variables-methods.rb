7415e6887f1626718909a921c20357e32631b25d
Michael Jasinski <majasinski8@gmail.com>
2015-10-01 00:39:13 -0500

    Add reflection to 4.3 file

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -1,3 +1,9 @@
+
+https://github.com/Michael-Jas/phase-0/blob/master/week-4/address/my_solution.rb
+
+https://github.com/Michael-Jas/phase-0/blob/master/week-4/math/my_solution.rb
+
+-------------------------------------------
 puts 'What is you first, middle, and last name?'
 
 name= gets.chomp
@@ -12,3 +18,37 @@ num= gets.chomp.to_i
 num += 1
 
 puts 'Well my Favorite ' + num.to_s
+
+----------------------------------------
+=begin
+How do you define a local variable?
+
+  A Local variable lives inside a method. The idea of the variable is that it allows flexibility and change to a method because it can hold different values. Since you can have only one specific method and you can call on a variety variables names to influence the method.
+How do you define a method?
+  It is an expression that can return a value. Also think of it as the verb of coding because it will do something.
+  ```
+  def my_dog
+    p Tortilla
+  end
+
+  dog
+```
+
+What is the difference between a local variable and a method?
+  The local variable lives in the method and acts as a descriptor that holds a value for the method. While the method is going to do something when it is called. var1 = 2 (var1 is the method and 2 is the variable)
+
+How do you run a ruby program from the command line?
+
+  Depending on the file name that has the .rb extension you would run in the command line...
+```
+ruby the_file_name.rb
+```
+How do you run an RSpec file from the command line?
+  If you are working on a .rb file and want to run the RSpec on the command ...
+```
+rspec  the_file_name.rspec
+```
+What was confusing about this material? What made sense?
+  I read the material and it all makes sense, but that was just reading it. When I had to do the exercises I had no idea where to satart. I am over thinking what is needed.
+
+=end
