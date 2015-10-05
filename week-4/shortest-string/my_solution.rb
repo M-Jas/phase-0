1cb3cff3770d8cc274f63b812fcbf3f2aa398eaf
Michael Jasinski <majasinski8@gmail.com>
2015-10-05 00:17:47 -0500

    Add 4.6/3 file

--- week-4/shortest-string/my_solution.rb
+++ week-4/shortest-string/my_solution.rb
@@ -1,6 +1,6 @@
 # Shortest String
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # shortest_string is a method that takes an array of strings as its input
 # and returns the shortest string
@@ -10,7 +10,39 @@
 #
 # If +list_of_words+ is empty the method should return nil
 
-#Your Solution Below
+#the question is set up the same as the last two
+#looking for shortest string in a list of words
+#I used [] but this is for a String
+
+#INPUT:A list of words "strings"
+#OUTPUT: The shortest string
+
+# def shortest_string(list_of_words)
+#   if list_of_words == []
+#     return nil
+#   end
+#   else
+#     list_of_words.each do |x| #telling each word in the array to do x
+#      if list_of_words[''] > x
+#         list_of_words[''] == x
+#      end
+#   end
+#     list_of_words
+# end
+# i either get no implicit conversion of String into Integer for this
+#if I take [''] out I get > as a undifiend method.
+
+
+#-----------------------------------
+
 def shortest_string(list_of_words)
-  # Your code goes here!
+  #if list_of_words == []
+     #return nil
+list_of_words.min {|a,b| a.length <=> b.length}
+#this looks more like they are comparing.
+
+ #list_of_words.min_by {|a| a.size}
+ #this one also works I and them both in irb and they check out.
+
+
 end
