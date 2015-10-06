421abbed811beeb39cf33c63098df0207d7b85af
Michael Jasinski <majasinski8@gmail.com>
2015-10-05 23:48:24 -0500

    add final for 4.6.5

--- week-4/concatenate-arrays/my_solution.rb
+++ week-4/concatenate-arrays/my_solution.rb
@@ -1,10 +1,34 @@
 # Concatenate Two Arrays
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself ].
+
+#INPUT: Takes two arrays as arugments
+#OUTPUT: Returns a single string
+# in all instants the array will combined into one bigger array
+#the method is taking two arguments arg1 and arg2
 
 
-# Your Solution Below
+#### THis was my first try below
+# def array_concat(array_1, array_2)
+#   x= nil
+#     if  array_1 >= x
+      #do array_1 + array_2
+#     elsif array_2 == x
+      # do array_1 + array_2
+
+  #end
+#end
+
+#WORKING______________________________
 
 def array_concat(array_1, array_2)
-  # Your code here
+
+   array_1 + array_2
+
 end
+
+# add arg1 + arg2
+#REFACTOR_____________________________
+def array_concat(array_1, array_2)
+     array_1.concat (array_2)
+ end
