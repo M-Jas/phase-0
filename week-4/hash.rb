b6904f20aa25cfdc667fd3631e1ad0fb421ea58c
Michael Jasinski <majasinski8@gmail.com>
2015-10-06 20:54:28 -0500

    Add hash my_info for 4.6.7

--- week-4/hash.rb
+++ week-4/hash.rb
@@ -1,12 +1,11 @@
 # Define a Hash
 
-# I worked on this challenge [by myself, with: ].
-
+# I worked on this challenge [by myself].
 
 # Your Solution Below
 
 # my_info is empty to start.  Edit this!
-my_info = {}
+my_info = {:first_name => "Michael", :last_name => "Jasinski", :hometown => "Arlington Heights", :age => 29,}
 
 
 
