45d6520fb07473318a1b8c44ad6389090dd2fefb
Michael Jasinski <majasinski8@gmail.com>
2015-10-08 23:38:59 -0500

    Add refactor to peer

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -23,49 +23,28 @@
 
 # 1. Initial Solution
 
-#created a method that takes and array as an argument
-def mode(array)
-#creating a new hash witha default value of zero
-  numbers = Hash.new 0
-#creat a array that can hold our biggest value
-  biggest_array = []
-#iterate through the array and set each new number as the key for the occurance. Then you will add 1 to value for each for each key
-  array.each do |item|
-    numbers[item] +=1
-  end
-#this is going to be the container that holds the values of the key. We sorted them last or biggest firs
-  biggest_array = numbers.values.sort!.last
-# We will delete all numbers that have less occurances
-  numbers.delete_if {|key, values| values < biggest_array}
-  #return the number with the most occurances
-  numbers.keys
-end
-array = [1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5]
-mode (array)
-# mode(array)
-#     look to see if the current item is equal to temp
-#     if it is then we know to add 1 more onto the current count for that number
-#     for example, if 1 is the current number set as temp
-#       and 1 is item, then we can increment count by 1
-#     if item = temp
-#       count += 1
-#     else
-# #       if the count of the current number is higher than the old highest, make it the new highest
-#       if count >= curr_hig
-#         curr_hig = temp
-#       end
-# #       now we want to reset the count and make the new item we are checking into our temporary
-#       count = 0
-#       temp = item
-#     end
+# #created a method that takes and array as an argument
+# def mode(array)
+# #creating a new hash witha default value of zero
+#   numbers = Hash.new 0
+# #creat a array that can hold our biggest value
+#   biggest_array = []
+# #iterate through the array and set each new number as the key for the occurance. Then you will add 1 to value for each for each key
+#   array.each do |item|
+#     numbers[item] +=1
 #   end
-#   temp.push (curr_hig)
+# #this is going to be the container that holds the values of the key. We sorted them last or biggest firs
+#   biggest_array = numbers.values.sort!.last
+# # We will delete all numbers that have less occurances
+#   numbers.delete_if {|key, values| values < biggest_array}
+#   #return the number with the most occurances
+#   numbers.keys
 # end
+# array = [1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5]
+# mode (array)
 
 
-
-
-
+#first trial___________________________________________________
 # def mode(array)
 #   # temp = 0
 #   curr_hig = 0
@@ -76,10 +55,17 @@ mode (array)
 #   end
 #   return curr_hig
 # end
-
+#______________________________________________________________
 # 3. Refactored Solution
+def mode(array)
+  array.each_with_object(Hash.new(0)){|numbers, item| numbers[item] += 1}
+  biggest_array = numbers.values.sort!.last
+  numbers.delete_if {|key, value| value < biggest_array}
+  end
+
+#I having and issue with condesing the code. This passes almost all of the test.
 
 
 
 
-# 4. Reflection
+# 4. Reflection_______________________________________________
