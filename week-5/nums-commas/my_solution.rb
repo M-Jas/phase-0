03aaafaccac05b53a7134cc1f92bd0d36e78e1fa
Michael Jasinski <majasinski8@gmail.com>
2015-10-11 02:14:33 -0500

    Start solo project 5_5

---
+++ week-5/nums-commas/my_solution.rb
@@ -0,0 +1,51 @@
+# Numbers to Commas Solo Challenge
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# 0. Pseudocode
+
+# INPUT: Positive Integer
+# OUTPUT: A postive interger with a comma to sperated integer and returned as string ex: "1,000"
+
+# What are the steps needed to solve the problem?
+  # Create a method called separate comma. The method takes a positive integer as argument
+  # I want argument to take in positive integers
+  # The integer can not be less then 0(negative number) it will fail
+  # When the number is less then 4 digits long return the number as a string
+  # When the number is greater then 3 digits long return the number as a string place a comma after the first digit.
+
+# 1. Initial Solution
+def separate_comma(integer)
+  num = integer.to_s
+  if
+    integer = rand(0..999)
+      return integer.to_s
+  elsif
+    integer = rand(1000..9999)
+      return integer.to_s
+  elsif
+    integer = rand(10000..99999)
+      return integer.to_s
+  elsif
+    integer = rand(100000..999999)
+      return integer.to_s
+  elsif
+    integer = rand(1000000..9999999)
+      return integer.to_s
+     elsif
+    integer = rand(1000000..9999999)
+      return integer.to_s
+  end
+end
+
+
+
+# 2. Refactored Solution
+
+
+
+# 3. Reflection
