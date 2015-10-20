dc8ed2d9c29bd68c1f619728a8a3903a5900c70a
Michael Jasinski <majasinski8@gmail.com>
2015-10-20 18:07:53 -0500

    Add solutions for reading

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -4,8 +4,12 @@
 
 // Program Structure
 // Write your own variable and do something to it.
-// var puppy = "Tortellini";
-// console.log(puppy + " " + "is my puppy")
+var puppy = "Tortellini";
+console.log(puppy + " " + "is my puppy")
+
+
+// var favFood = prompt("what is your favorite food?");
+
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
 //Triangle
@@ -21,8 +25,26 @@ var hashTag = "#";
 // Functions
 
 // Complete the `minimum` exercise.
+  var myVar = function(a, b) {
+    if (a < b ) {
+      return a;
+    };
+  }
 
-
+  console.log(myVar(8, 12));
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+
+var me = {
+  name: "Michael",
+  age: 28,
+  fav_food: ["Softshell Crab", "Tacos", "Buffalo wings"],
+  quirk: "I was cast as a leading role in Little Rascals Movie"
+};
+
+console.log(me.age)
+console.log(me.name)
+console.log(me.fav_food)
+console.log(me.quirk)
