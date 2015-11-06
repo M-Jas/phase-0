89ca53ce7a89e29948d2d4f38c0d0760ddd141d7
Michael Jasinski <majasinski8@gmail.com>
2015-11-06 13:05:49 -0600

    Add fib files

---
+++ week-9/ruby/fibonacci.rb
@@ -0,0 +1,28 @@
+# I worked on this challenge [by myself, with: ].
+# This challenge took me [#] hours.
+
+# Pseudocode
+
+
+
+# Initial Solution
+
+def is_fibonacci?(num)
+  if num <= 9
+    true
+  elsif
+  end
+
+end
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+
+# Reflection
